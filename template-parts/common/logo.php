25ed4b24d23f514e90de35e87296b32dcad7d24a
lauraarnaudo <laura.arnaudo@technicaldesign.it>
2025-09-23 15:21:04 +0200

    Miglioria accessibilità "Logo"

--- template-parts/common/logo.php
+++ template-parts/common/logo.php
@@ -1,7 +1,7 @@
 <?php
 if (dci_get_option("stemma_comune")) {
 ?>
-<svg width="82" height="82" class="icon" aria-hidden="true">       
+<svg width="82" height="82" class="icon" aria-label="Logo">       
      <image xlink:href="<?php echo dci_get_option("stemma_comune");?>" width="82" height="82"/>    
 </svg>
 <?php } ?>
